5ed2341a1a5f0049d26a8321694b26f90315e76b
gracjan <gracjan.jankowski@gmail.com>
2018-08-22 16:51:58 +0200

    ci integrations tests (11)

--- src/main/kotlin/nsl/sam/spring/config/BasicAuthConfig.kt
+++ src/main/kotlin/nsl/sam/spring/config/BasicAuthConfig.kt
@@ -15,7 +15,7 @@ import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.web.AuthenticationEntryPoint
 
 @Configuration
-class BasicAuthConfig {
+class_ BasicAuthConfig {
 
     @Autowired
     @Qualifier("unauthenticatedAccessResponseSender")
