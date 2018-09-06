41b9320ec61064aa925f7641db56a45ca90912af
gracjan <gracjan.jankowski@gmail.com>
2018-09-06 15:39:00 +0200

    mockito driven unit tests for EnabledEntrypointsSelector

---
+++ src/test/kotlin/nsl/sam/spring/annotation/EnabledEntrypointsSelectorTest.kt
@@ -0,0 +1,182 @@
+package nsl.sam.spring.annotation
+
+import nsl.sam.spring.config.BasicAuthConfig
+import nsl.sam.spring.config.DisableBasicAuthConfig
+import nsl.sam.spring.config.SimpleWebSecurityConfigurer
+import nsl.sam.spring.config.TokenAuthConfig
+import org.assertj.core.api.Assertions
+import org.junit.Test
+
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.springframework.core.type.AnnotationMetadata
+import org.mockito.BDDMockito.*
+
+@RunWith(MockitoJUnitRunner::class)
+class EnabledEntrypointsSelectorTest {
+
+    @Mock
+    lateinit var importingClassMetadata: AnnotationMetadata
+
+    @Test
+    fun `selectImports(), SIMPLE_BASIC_AUTH enabled`() {
+
+        // MOCKITO FIXTURES
+        given(
+                importingClassMetadata.getAnnotationAttributes(
+                        EnableSimpleAuthenticationMethods::class.qualifiedName!!, false
+                )
+        ).willReturn(
+                mapOf("methods" to arrayOf(AuthenticationMethod.SIMPLE_BASIC_AUTH))
+        )
+
+        // OBJECT OF TESTED CLASS
+        val enabledEntrypointsSelector = EnabledEntrypointsSelector()
+
+        // ACT
+        val configurationClasses = enabledEntrypointsSelector.selectImports(importingClassMetadata)
+
+        // ASSERT
+        Assertions.assertThat(configurationClasses).containsExactlyInAnyOrder(
+                SimpleWebSecurityConfigurer::class.qualifiedName,
+                BasicAuthConfig::class.qualifiedName
+        )
+    }
+
+    @Test
+    fun `selectImports(), SIMPLE_TOKEN enabled`() {
+
+        // MOCKITO FIXTURES
+        given(
+                importingClassMetadata.getAnnotationAttributes(
+                        EnableSimpleAuthenticationMethods::class.qualifiedName!!, false
+                )
+        ).willReturn(
+                mapOf("methods" to arrayOf(AuthenticationMethod.SIMPLE_TOKEN))
+        )
+
+        // OBJECT OF TESTED CLASS
+        val enabledEntrypointsSelector = EnabledEntrypointsSelector()
+
+        // ACT
+        val configurationClasses = enabledEntrypointsSelector.selectImports(importingClassMetadata)
+
+        // ASSERT
+        Assertions.assertThat(configurationClasses).containsExactlyInAnyOrder(
+                SimpleWebSecurityConfigurer::class.qualifiedName,
+                TokenAuthConfig::class.qualifiedName,
+                DisableBasicAuthConfig::class.qualifiedName
+        )
+    }
+
+    @Test
+    fun `selectImports(), SIMPLE_BASIC_AUTH and SIMPLE_TOKEN enabled`() {
+
+        // MOCKITO FIXTURES
+        given(
+                importingClassMetadata.getAnnotationAttributes(
+                        EnableSimpleAuthenticationMethods::class.qualifiedName!!, false
+                )
+        ).willReturn(
+                mapOf("methods" to arrayOf(
+                        AuthenticationMethod.SIMPLE_TOKEN, AuthenticationMethod.SIMPLE_BASIC_AUTH))
+        )
+
+        // OBJECT OF TESTED CLASS
+        val enabledEntrypointsSelector = EnabledEntrypointsSelector()
+
+        // ACT
+        val configurationClasses = enabledEntrypointsSelector.selectImports(importingClassMetadata)
+
+        // ASSERT
+        Assertions.assertThat(configurationClasses).containsExactlyInAnyOrder(
+                SimpleWebSecurityConfigurer::class.qualifiedName,
+                TokenAuthConfig::class.qualifiedName,
+                BasicAuthConfig::class.qualifiedName
+        )
+    }
+
+    @Test
+    fun `selectImports(), SIMPLE_NO_METHOD enabled`() {
+
+        // MOCKITO FIXTURES
+        given(
+                importingClassMetadata.getAnnotationAttributes(
+                        EnableSimpleAuthenticationMethods::class.qualifiedName!!, false
+                )
+        ).willReturn(
+                mapOf("methods" to arrayOf(
+                        AuthenticationMethod.SIMPLE_NO_METHOD))
+        )
+
+        // OBJECT OF TESTED CLASS
+        val enabledEntrypointsSelector = EnabledEntrypointsSelector()
+
+        // ACT
+        val configurationClasses = enabledEntrypointsSelector.selectImports(importingClassMetadata)
+
+        // ASSERT
+        Assertions.assertThat(configurationClasses).containsExactlyInAnyOrder(
+                SimpleWebSecurityConfigurer::class.qualifiedName,
+                DisableBasicAuthConfig::class.qualifiedName
+        )
+    }
+
+    @Test
+    fun `selectImports(), SIMPLE_NO_METHOD and SIMPLE_TOKEN and SIMPLE_BASIC_AUTH enabled`() {
+
+        // MOCKITO FIXTURES
+        given(
+                importingClassMetadata.getAnnotationAttributes(
+                        EnableSimpleAuthenticationMethods::class.qualifiedName!!, false
+                )
+        ).willReturn(
+                mapOf("methods" to arrayOf(
+                        AuthenticationMethod.SIMPLE_NO_METHOD,
+                        AuthenticationMethod.SIMPLE_TOKEN,
+                        AuthenticationMethod.SIMPLE_BASIC_AUTH))
+        )
+
+        // OBJECT OF TESTED CLASS
+        val enabledEntrypointsSelector = EnabledEntrypointsSelector()
+
+        // ACT
+        val configurationClasses = enabledEntrypointsSelector.selectImports(importingClassMetadata)
+
+        // ASSERT
+        Assertions.assertThat(configurationClasses).containsExactlyInAnyOrder(
+                SimpleWebSecurityConfigurer::class.qualifiedName,
+                DisableBasicAuthConfig::class.qualifiedName
+        )
+    }
+
+    @Test
+    fun `selectImports(), SIMPLE_BASIC_AUTH and SIMPLE_METHOD and SIMPLE_NO_METHOD enabled`() {
+
+        // MOCKITO FIXTURES
+        given(
+                importingClassMetadata.getAnnotationAttributes(
+                        EnableSimpleAuthenticationMethods::class.qualifiedName!!, false
+                )
+        ).willReturn(
+                mapOf("methods" to arrayOf(
+                        AuthenticationMethod.SIMPLE_BASIC_AUTH,
+                        AuthenticationMethod.SIMPLE_TOKEN,
+                        AuthenticationMethod.SIMPLE_NO_METHOD))
+        )
+
+        // OBJECT OF TESTED CLASS
+        val enabledEntrypointsSelector = EnabledEntrypointsSelector()
+
+        // ACT
+        val configurationClasses = enabledEntrypointsSelector.selectImports(importingClassMetadata)
+
+        // ASSERT
+        Assertions.assertThat(configurationClasses).containsExactlyInAnyOrder(
+                SimpleWebSecurityConfigurer::class.qualifiedName,
+                DisableBasicAuthConfig::class.qualifiedName
+        )
+    }
+
+}
