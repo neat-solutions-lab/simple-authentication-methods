a03edb29afb281a8f2ef96f993e8996119ceaaf8
gracjan <gracjan.jankowski@gmail.com>
2018-09-21 13:54:01 +0200

    before annotation package movement

--- src/main/kotlin/nsl/sam/method/basicauth/annotation/SimpleBasicAuthenticationAttributes.kt
+++ src/main/kotlin/nsl/sam/method/basicauth/annotation/SimpleBasicAuthenticationAttributes.kt
@@ -1,7 +1,32 @@
 package nsl.sam.method.basicauth.annotation
 
-class SimpleBasicAuthenticationAttributes {
+import org.springframework.boot.autoconfigure.info.ProjectInfoProperties
 
+data class SimpleBasicAuthenticationAttributes(
+        val passwordsFilePathProperty: String = "",
+        val passwordsFilePath: String = ""
+) {
 
+    companion object {
+        fun create(init: Builder.()->Unit) = Builder(init).build()
+    }
+
+    class Builder private constructor() {
+
+        constructor(init: Builder.()->Unit):this() {
+            init()
+        }
+
+        var passwordsFilePathProperty = ""
+        var passwordsFilePath = ""
+
+        fun passwordsFilePathProperty(passwordsFilePathProperty: ()->String) =
+                apply { this.passwordsFilePathProperty = passwordsFilePathProperty() }
+
+        fun passwordsFilePath(passwordsFilePath: ()->String) =
+                apply { this.passwordsFilePath = passwordsFilePath() }
+
+        fun build() = SimpleBasicAuthenticationAttributes(this.passwordsFilePathProperty, this.passwordsFilePath)
+    }
 
 }
