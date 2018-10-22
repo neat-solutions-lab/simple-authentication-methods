fdd716855994eba363e77bd686e76451d94d9b13
gracjan <gracjan.jankowski@gmail.com>
2018-10-22 08:16:04 +0200

    tests for ConditionalREadWriteSynchronizer

--- src/test/kotlin/nsl/sam/importer/reader/FileCredentialsReaderTest.kt
+++ src/test/kotlin/nsl/sam/importer/reader/FileCredentialsReaderTest.kt
@@ -6,31 +6,18 @@ import org.junit.jupiter.api.Test
 
 class FileCredentialsReaderTest {
 
-
-
-
     @Test
-    fun underlyingFileCanBeRemovedOnlyAfterFileCredentialsReaderIsClosed() {
+    fun underlyingFileCanBeRemovedAfterFileCredentialsReaderIsClosed() {
+
         val tmpFile = createTempFile()
         val fileCredentialsReader = FileCredentialsReader(tmpFile.absolutePath)
         fileCredentialsReader.reset()
         fileCredentialsReader.readCredentials()
-        var deleteResult = tmpFile.delete()
-        println("delete result: $deleteResult")
 
-        /*
-         * as the FileCredentialsReader has not been closed, the tmpFile cannot be deleted
-         */
-        Assertions.assertThat(deleteResult).isEqualTo(false)
 
         fileCredentialsReader.close()
-        deleteResult = tmpFile.delete()
+        val deleteResult = tmpFile.delete()
 
-        /*
-         * now the file could be removed (becuae FileCredentialsReader has been closed)
-         */
         Assertions.assertThat(deleteResult).isEqualTo(true)
-
     }
-
 }
